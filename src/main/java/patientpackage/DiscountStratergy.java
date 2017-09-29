5b42beaa67b015a6a9e484844a6770e5ffd29a8a
Ahmed <Ahmed@ahmeds-mbp.lan>
2017-09-29 22:38:55 +0100

    discountstratergy

--- src/main/java/patientpackage/DiscountStratergy.java
+++ src/main/java/patientpackage/DiscountStratergy.java
@@ -1,43 +1,42 @@
+package patientpackage;
 
 public interface DiscountStratergy {
 
 	double hasDiscount();
 }
 
-
 class ChildDiscountStratergy implements DiscountStratergy {
 
 	public double hasDiscount() {
-		
+
 		return 0.4;
 	}
-	
+
 }
 
-class Over70DiscountStratergy implements DiscountStratergy{
+class Over70DiscountStratergy implements DiscountStratergy {
 
 	public double hasDiscount() {
-		
+
 		return 0.9;
 	}
-	
+
 }
 
-class Between6570DiscountStratergy implements DiscountStratergy{
+class Between6570DiscountStratergy implements DiscountStratergy {
 
 	public double hasDiscount() {
-		
+
 		return 0.6;
 	}
-	
+
 }
 
 class StandardDiscountStratergy implements DiscountStratergy {
 
 	public double hasDiscount() {
-		
+
 		return 0.0;
 	}
-	
-}
 
+}
