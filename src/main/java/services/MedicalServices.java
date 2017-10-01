27aca0cb69e2c737d204908b4abf5a15fb998a81
Ahmed <Ahmed@ahmeds-mbp.lan>
2017-10-01 14:53:09 +0100

    final price field in enum

--- src/main/java/services/MedicalServices.java
+++ src/main/java/services/MedicalServices.java
@@ -3,9 +3,9 @@ package services;
 public enum MedicalServices {
 	XRAY(150.00), DIAGNOSIS(60.00), BLOODTEST(78.00), ECG(200.40), VACCINE(27.50);
 
-	private double price;
+	private final double price;
 
-	private MedicalServices(final double price) {
+	private MedicalServices(double price) {
 		this.price = price;
 	}
 
